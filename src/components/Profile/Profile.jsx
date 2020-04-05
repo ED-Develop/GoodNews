3d4393bb2f10695faafee6d2b1c9bc895be432c3
Edward Shvetsov <edward.shvetsov@anvileight.com>
2020-04-05 17:57:07 +0300

    Create profile layout

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,14 +6,19 @@ import profileImage from '../../assets/image/user.jpg';
 const Profile = ({userName}) => {
     return (
         <div className={style.profileWrapper}>
-           <Row className='d-flex justify-content-center'>
-               <Col md={4} className={style.profileImage}>
-                   <img src={profileImage} alt="image"/>
-               </Col>
-               <Col md={6} className={style.userInfo}>
-                    <h2>Admin</h2>
-               </Col>
-           </Row>
+            <div className={style.profileContainer}>
+                <Row>
+                    <Col md={4} className={style.profileImage}>
+                        <img src={profileImage} alt="image"/>
+                    </Col>
+                    <Col md={8} className={style.userInfo}>
+                        <h2>Admin</h2>
+                        <p><span>Date of Birth:</span> 26.07.1999</p>
+                        <p><span>Your tariff plan:</span> trial for 30 days</p>
+                        <p><span>Your news region:</span> USA</p>
+                    </Col>
+                </Row>
+            </div>
         </div>
     )
 };
